6eace74c5997eae0a60f6a2774fce6d861b5ca84
da-munchies24 <91292427+da-munchies24@users.noreply.github.com>
2021-09-25 20:57:55 -0400

    Update Board.cpp

--- HW5_CS_2303/src/Board.cpp
+++ HW5_CS_2303/src/Board.cpp
@@ -71,5 +71,53 @@ void Board::setEdgesP(char* set)
 char* Board::getEdgesP(){
 	return (*this).edgesP;
 }
-
+void Board::moveb()
+{
+	int right = 0, left = 0, up = 0;
+		//check if can jump
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 8; j++) {
+				if (this->getEdge(i,j) == 'b') {
+					up = i - 1; right = j + 1; left = j - 1;
+					if (up >= 0)
+					{
+						if (right >= 1 && right <= 8) {
+							if (this->getEdge(up,right)== 'r')
+							{
+								up -= 1;
+								right += 1;
+								if (up >= 0 && right >= 1 && right <= 8)
+								{
+									if(this->getEdge(up,right)=='-')
+									{
+										this->setEdge(up, right, 'b');
+										this->setEdge((up+1), (right-1), '-');
+										this->setEdge(i,j,'-');
+									}
+								}
+							}
+						} // End if right
+						if (left >= 0 && left <= 7)
+						{
+							if (this->getEdge(up,left) == 'r')
+							{
+								up -= 1;
+								left -= 1;
+								if (up >= 0 && left >= 0 && left <= 7)
+								{
+									puts("Can jump on left");
+									if(this->getEdge(up,left)=='-'){
+									this->setEdge(up, left, 'b');
+									this->setEdge((up+1),(left+1),'-');
+									this->setEdge(i,j,'-');
+									}
+								}
+							}
+						} // End if left
+					}// End if up
+				} // End if b
+			} // End for col
+		} // End for row
+}
 
