ed6c73829421fc8056fdc2c2c6becd5f4735fb27
Tim-K3007 <77994355+Tim-K3007@users.noreply.github.com>
2021-09-25 22:07:13 -0400

    Update Board.cpp

--- HW5_CS_2303/src/Board.cpp
+++ HW5_CS_2303/src/Board.cpp
@@ -19,35 +19,42 @@ Board::~Board() {
 void Board::displayBoard()
 {
 	//TODO
+	ofstream boards;
+	boards.open("boards.txt", ios_base::app);
 	printf("  0 1 2 3 4 5 6 7 \n");
+	boards<<"  0 1 2 3 4 5 6 7 \n";
 	for(int i = 0; i<8; i++)
 		{
 			printf("%d", i);
+			boards<<i;
 			for(int j = 0; j<8; j++)
 			{
 				printf(" %c",(*this).getEdge(i, j));
-
+				boards<<" "<<(*this).getEdge(i, j);
 			}
 			printf("\n");
+		    boards<< "\n";
 		}
+	boards.close();
 }
-void Board::printToFile(char* filename)
-{
-    //TODO
-	ofstream fp;
-	fp.open(filename);
-	int ncols=n;
-	for(int row = 0; row<ncols; row++)
-		{
-			for(int col = 0; col<ncols; col++)
-			{
-				fp<<*((edgesP)+(row*ncols)+col)<<" ";
-			}
-			fp<<"\n";
-		}
-	fp.close();
-	}
-}
+
+//void Board::printToFile(char* filename)
+//{
+//    //TODO
+//	ofstream fp;
+//	fp.open(filename);
+//	int ncols=n;
+//	for(int row = 0; row<ncols; row++)
+//		{
+//			for(int col = 0; col<ncols; col++)
+//			{
+//				fp<<*((edgesP)+(row*ncols)+col)<<" ";
+//			}
+//			fp<<"\n";
+//		}
+//	fp.close();
+//}
+
 void Board::init()
 {
 	int ncols = n;
