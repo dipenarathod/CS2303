ebdfe066defe1e3c0df04a96696c9ef4371c346e
Tim-K3007 <77994355+Tim-K3007@users.noreply.github.com>
2021-09-25 21:12:10 -0400

    Update Board.cpp

--- HW5_CS_2303/src/Board.cpp
+++ HW5_CS_2303/src/Board.cpp
@@ -19,11 +19,13 @@ Board::~Board() {
 void Board::displayBoard()
 {
 	//TODO
+	printf("  0 1 2 3 4 5 6 7 \n");
 	for(int i = 0; i<8; i++)
 		{
+			printf("%d", i);
 			for(int j = 0; j<8; j++)
 			{
-				printf("%c",(*this).getEdge(i, j));
+				printf(" %c",(*this).getEdge(i, j));
 
 			}
 			printf("\n");
