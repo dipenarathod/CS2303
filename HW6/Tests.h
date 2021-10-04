f7e01597e9ee7c4a720099bfaff193cf371780ef
dipenarathod <30725272+dipenarathod@users.noreply.github.com>
2021-10-03 22:23:37 -0400

    Update Tests.h

--- HW6/Tests.h
+++ HW6/Tests.h
@@ -10,18 +10,29 @@
 
 #include "Production.h"
 #include "Board.h"
+#include <iostream>
 class Tests {
 public:
 	Tests();
 	virtual ~Tests();
 	bool tests();
+	//Production functions
 	bool testWelcomeScreen();
 	bool testSystemMessage();
+
+	//Board Functions
 	bool testInitializeGameBoard();
 	bool testPrintGameBoard();
 	bool testManuallyPlaceShipsOnGameBoard();
 	bool testRandomlyPlaceShipsOnGameBoard();
 	bool testUpdateGameBoard();
+
+	//Fleet Functions
+	bool testCheckBoundsOfCardinal();
+	bool testCheckSunkShip();
+	bool testIsWinner();
+	bool testGetTarget();
+	bool testCheckShot();
 };
 
 #endif /* TESTS_H_ */
